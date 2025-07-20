b8372f77820ffe2af2634cc12e9b790e235ae47e
tiffz <zhangtiff@gmail.com>
2025-07-20 15:01:22 -0700

    fix deplolyment for real

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: resolve(__dirname, 'CNAME'),
+          dest: '.',
+        },
+      ],
+    }),
+  ],
   root: 'src',
   build: {
     outDir: '../dist',
