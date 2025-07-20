646008f3e76fbbc844400dc655faca93cd9c0708
tiffz <zhangtiff@gmail.com>
2025-07-20 14:34:16 -0700

    adjust setup a bit

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: 'pretty-url-rewrite',
-      configureServer(server) {
-        server.middlewares.use((req, _res, next) => {
-          if (req.url === '/zines') {
-            req.url = '/zines/index.html';
-          }
-          next();
-        });
-      },
-    },
-  ],
-  base: '/',
-  server: {
-    port: 8080,
-    fs: { allow: [process.cwd()] },
-  },
+  plugins: [react()],
+  root: 'src',
   build: {
-    outDir: 'build',
+    outDir: '../dist',
     rollupOptions: {
       input: {
-        main: path.resolve(process.cwd(), 'index.html'),
-        zines: path.resolve(process.cwd(), 'zines/index.html'),
+        main: resolve(__dirname, 'src/index.html'),
+        zines: resolve(__dirname, 'src/zines/index.html'),
       },
     },
   },
