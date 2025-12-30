8dfe7b66a1828c67510f288d1fd159578001b76a
tiffz <zhangtiff@gmail.com>
2025-12-29 20:48:08 -0800

    add more common patterns

--- src/drums/data/commonPatterns.ts
+++ src/drums/data/commonPatterns.ts
@@ -2,16 +2,22 @@
  * Common Patterns
  * 
  * Frequently used rhythm patterns that appear in the note palette for quick insertion.
+ * Patterns are grouped so that patterns with the same rhythm structure (but different drum sounds) are together.
  */
-
 export const COMMON_PATTERNS: string[] = [
   'DKTK',
   'TKTK',
   'D-K-',
+  'D-D-',
   'T-K-',
   'D-TK',
+  'T-TK',
+  'TKT-',
+  'DKD-',
+  'DK-T',
+  'D--K',
+  'DK--',
   '__D-',
   '__K-',
   '__T-',
-  'T-TK',
 ];
